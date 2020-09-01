0b14d69172d4e74919559e20834a42b8dadfdbf7
bzhu <bicen.zhu@nokia-sbell.com>
2020-09-01 17:32:42 +0800

    oauth2-client in progress

--- oauth2-client/src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
+++ oauth2-client/src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
@@ -2,12 +2,22 @@ package com.example.oauth2client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableResourceServer
 @SpringBootApplication
+@RestController
 public class Oauth2ClientApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2ClientApplication.class, args);
+	}
+
+	@GetMapping("/")
+	String hello () {
+		return "Hello";
 	}
 
 }
